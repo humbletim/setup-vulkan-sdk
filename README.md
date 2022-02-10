b31ac9320f9943ca36b498d02094c78518b7f273
humbletim <humbletim@gmail.com>
2022-02-10 03:07:49 -0500

    + support for SDK components and prebuilts

--- README.md
+++ README.md
@@ -1,48 +1,131 @@
-# setup-vulkan-sdk v1.0.3
+# setup-vulkan-sdk v1.1.0
 
 <p align="left">
   <a href="https://github.com/humbletim/setup-vulkan-sdk"><img alt="GitHub Actions status" src="https://github.com/humbletim/setup-vulkan-sdk/workflows/Setup/badge.svg"></a>
 </p>
 
-This action builds a subset of the Vulkan SDK from source and makes the results available to later build steps through a `VULKAN_SDK` environment variable.
+This action installs the Vulkan SDK components needed to compile and link Vulkan applications.
 
-*note: currently only x64 windows, x64 linux and macOS environments are supported (if you need another configuration added let me know)*
+By default only the API headers and loader are included, but other components can also be selected using action parameters.
 
-# Basic Usage
+One of the challenges around building Vulkan SDK components from source is determining compatible versions across Khronos repos. Fortunately, LunarG provides a useful API web service that this action leverages to resolve release numbers into corresponding source. For production workflows, it is however recommended to create a project-local snapshot of the SDK revision specs (see last integration example below).
 
-To build against the latest Vulkan SDK:
+## Usage
+
+_note: if new to GitHub Actions please see GitHub Help Documentation [Quickstart](https://docs.github.com/en/actions/quickstart) or [Creating a workflow file](https://docs.github.com/en/actions/using-workflows#creating-a-workflow-file)._
+
+### Default integration
+
+To use the latest release versions of Vulkan SDK headers and loader:
 ```yaml
   -name: Install Vulkan SDK
-   uses: humbletim/setup-vulkan-sdk@v1.0.3
+   uses: humbletim/setup-vulkan-sdk@v1.1.0
 ```
 
-## Parameters
-
-- `vulkan-version`:
-*(Optional)* valid SDK release number (eg: 1.2.161.1) or git branch (eg: sdk-1.2.161). Default: `latest`
-    - Available release numbers can be found here: https://vulkan.lunarg.com/sdk/home
-    - Note: sdk-x.y.z branches are used directly whereas release numbers get resolved through Vulkan SDK webservices
-
-## Specifying an exact SDK by *packaged release number*:
+As of this moment, the `latest` SDK release is `1.2.198.1` -- so expanding default values the above is equivalent to:
 ```yaml
-  -name: Install Vulkan SDK
-   uses: humbletim/setup-vulkan-sdk@v1.0.3
-   with:
-     vulkan-version: 1.2.161.1
+  - name: Install Vulkan SDK
+    uses: humbletim/setup-vulkan-sdk@v1.1.0
+    with:
+      vulkan-version: 1.2.198.1
+      vulkan-components: headers, loader
+      vulkan-use-cache: false
 ```
 
-## Specifying an exact SDK by *khronos git branch*:
+### Custom integration
+To target a different Vulkan SDK release number, include glslangValidator tooling, and enable caching across repeated action runs:
 ```yaml
-  -name: Install Vulkan SDK
-   uses: humbletim/setup-vulkan-sdk@v1.0.3
-   with:
-     vulkan-version: sdk-1.1.108
+  - name: Install Vulkan SDK
+    uses: humbletim/setup-vulkan-sdk@v1.1.0
+    with:
+      vulkan-version: 1.2.161.1
+      vulkan-components: headers, loader, glslang
+      vulkan-use-cache: true
 ```
 
-## How it works
+### Advanced integration
+One way to lock-in a specific SDK version spec is to add the corresponding `config.json` to your project tree and specify its path as the `vulkan-config-file` parameter.
 
-First [action.yml](action.yml) installs the minimum dependencies needed to compile Vulkan SDK from source. It then delegates to [install_vulkan_sdk.sh](install_vulkan_sdk.sh), which if needed resolves `vulkan-version` into a corresponding git branch using the official Vulkan SDK [web services](https://vulkan.lunarg.com/content/view/latest-sdk-version-api).
+Here is an example SDK spec for the Linux v1.2.198.1 Vulkan SDK release: https://vulkan.lunarg.com/sdk/config/1.2.198.1/linux/config.json
 
-[KhronosGroup/Vulkan-Headers](https://github.com/KhronosGroup/Vulkan-Headers) and [KhronosGroup/Vulkan-Loader](https://github.com/KhronosGroup/Vulkan-Loader) repos are then cloned locally to that branch point, built, and installed into `$PWD/VULKAN_SDK`. And finally a `VULKAN_SDK` environment variable is exported for later build tools to discover the SDK's location through.
+Example steps for adding to the local project tree:
+```sh
+mkdir vulkan_sdk_specs
+curl -o vulkan_sdk_specs/linux.json https://vulkan.lunarg.com/sdk/config/1.2.198.1/linux/config.json
+git add vulkan_sdk_specs/linux.json
+git commit -m "pinned vulkan sdk version for linux"
+```
+Example Workflow integration:
+```yaml
+  - name: Configure Vulkan SDK using in-repo config
+    uses: humbletim/setup-vulkan-sdk@v1.1.0
+    with:
+      vulkan-config-file: vulkan_sdk_specs/linux.json
+      vulkan-components: headers, loader
+      vulkan-use-cache: true
+```
 
-The entire process takes around a minute or so to complete and the installed artifacts consume around a dozen megabytes of storage space (as opposed to the 1GB+ required for a full Vulkan SDK prebuilt install). Note that this approach only provides enough to compile and link Vulkan applications against -- it _does not_ provide the other utilities, documentation, or code samples that normally ship with the [Full Vulkan SDK](https://www.lunarg.com/vulkan-sdk/).
+## Action Parameters
+
+- `vulkan-version`: *(optional)* valid SDK release number (eg: 1.2.161.1). Default: `latest`.
+    - Officially supported release numbers can be found here: https://vulkan.lunarg.com/sdk/home
+- `vulkan-config-file`: *(optional)* project-local config.json file path. (note: this will override `vulkan-version` if both are specified)
+    - Documentation on querying config.json SDK specs can be found here: https://vulkan.lunarg.com/content/view/latest-sdk-version-api
+- `vulkan-use-cache`: *(optional)* if `true` VULKAN_SDK will be automatically cached and restored across repeat builds (using [actions/cache](https://github.com/actions/cache)). Default: `false`.
+    - note: cache is unique per each runner operating system + vulkan-* action parameters combination.
+- `vulkan-components`: *(optional)* individual Vulkan component selections. Default: `headers, loader`.
+  - Available options:
+    - `headers` - [KhronosGroup/Vulkan-Headers](https://github.com/KhronosGroup/Vulkan-Headers)
+    - `loader` - [KhronosGroup/Vulkan-Loader](https://github.com/KhronosGroup/Vulkan-Loader)
+    - `glslang` - [KhronosGroup/Glslang](https://github.com/KhronosGroup/Glslang)
+    - `spirv-cross` - [KhronosGroup/SPIRV-Cross](https://github.com/KhronosGroup/SPIRV-Cross)
+    - `spirv-tools` - [KhronosGroup/SPIRV-Tools](https://github.com/KhronosGroup/SPIRV-Tools)
+    - `spirv-reflect` - [KhronosGroup/SPIRV-Reflect](https://github.com/KhronosGroup/SPIRV-Reflect)
+    - `spirv-headers` - [KhronosGroup/SPIRV-Headers](https://github.com/KhronosGroup/SPIRV-Headers)
+    - `validation` - [KhronosGroup/Vulkan-ValidationLayers](https://github.com/KhronosGroup/Vulkan-ValidationLayers)
+
+## Notes
+
+As of v1.1.0 logic has been consolidated into [install_vulkan_sdk.sh](install_vulkan_sdk.sh), which could also be used standalone for local installs or as part of similar CI/CD build environments:
+```sh
+# show available command-line arguments
+./install_vulkan_sdk.sh --help
+
+# query Vulkan webservice for available SDK versions
+./install_vulkan_sdk.sh --query-versions
+
+# query corresponding SDK component repos/refs specific to an SDK release
+./install_vulkan_sdk.sh 1.2.161.1 --query-config
+
+# install a specific SDK version (saving VULKAN_SDK environment var into 'output.env')
+env GITHUB_ENV=output.env ./install_vulkan_sdk.sh 1.2.161.1 headers loader
+. output.env
+echo "VULKAN_SDK=$VULKAN_SDK"
+```
+
+Additional action integration examples can be found as part of this project's CI test suite: [.github/workflows/ci.yml](.github/workflows/ci.yml).
+
+## References
+- [Vulkan SDK](https://www.lunarg.com/vulkan-sdk/)
+- [Vulkan SDK web services API](https://vulkan.lunarg.com/content/view/latest-sdk-version-api)
+
+------------------------------
+
+## Experimental
+
+### Using prebuilt Vulkan SDKs
+
+This action now also supports unattended installation of official Vulkan SDK binary releases; however, this is only meant only as a fallback strategy to consider alongside building just the components you need from source. Note that SDK binary releases currently include over a gigabyte of expanded content that is typically unnecessary for automated builds.
+
+Addition ~component parameter option:
+- `prebuilt` - [LunarG Vulkan SDK binaries](https://www.lunarg.com/vulkan-sdk/) (automated download/unpacking)
+  - _note: the prebuilt option is meant to be mutually-exclusive with all other options except loader_
+
+To perform an unattended install of a specific Vulkan SDK binary release:
+```yaml
+  - name: Install Monolithic Vulkan SDK binary release
+    uses: humbletim/setup-vulkan-sdk@v1.1.0
+    with:
+      vulkan-version: 1.2.161.1
+      vulkan-components: prebuilt
+```
--- install_vulkan_sdk.sh
+++ install_vulkan_sdk.sh
@@ -1,108 +1,388 @@
 #!/bin/bash
 # helper script to clone, build and install the Vulkan SDK on headless CI systems
 # currently outputs into VULKAN_SDK in the current directory
+# depends on typical github actions environment tools (bash, grep, jq, curl, awk)
+
 # 2021.02 humbletim -- released under the MIT license
+# 2022.02 humbletim -- revamped to support components and prebuilt SDKs
 
-# usage: ./install_vulkan_sdk.sh <SDK_release_version | sdk-x.y.z>
-#    eg: ./install_vulkan_sdk.sh 1.2.162.1 # branch resolved via service
-#    eg: ./install_vulkan_sdk.sh sdk-1.2.162 # branch used directly
-
-# log messages will be printed to STDERR (>&2)
-# these sourcable environment variables will be printed to STDOUT on success:
-#   VULKAN_SDK=...
-#   VULKAN_SDK_VERSION=...
-
-test -d VULKAN_SDK || mkdir VULKAN_SDK || true
+__version__=1.1.0-alpha
 
 set -e
 
-VK_VERSION=${1:-latest}
-
 os=unknown
-build_dir=$PWD
 case `uname -s` in
-  Darwin)
-    os=mac
-    ;;
-  Linux)
-    os=linux
-    ;;
-  MINGW*)
-    os=windows
-    CC=cl.exe
-    CXX=cl.exe
-    PreferredToolArchitecture=x64
-    build_dir=$(pwd -W)
-    unset TEMP
-    unset TMP
-    ;;
+  Darwin) os=mac ext=dmg build_dir=$PWD ;;
+  Linux) os=linux ext=tar.gz build_dir=$PWD ;;
+  MINGW*) os=windows ext=exe build_dir=$(pwd -W) ;;
 esac
-echo os=$os >&2
-echo build_dir=$build_dir >&2
 
-# resolve latest into an actual SDK release number (currently only used for troubleshooting / debug output)
-REAL_VK_VERSION=$VK_VERSION
-if [[ $VK_VERSION == latest ]] ; then
-  REAL_VK_VERSION=$(curl -s https://vulkan.lunarg.com/sdk/latest.json | jq .$os --raw-output)
-  echo "resolved $VK_VERSION=$REAL_VK_VERSION" >&2
+if [[ "$*" == *--help ]] ; then
+  echo "Vulkan SDK install helper v$__version__"
+  echo ""
+  echo "usage for SDK version:"
+  echo "   ./install_vulkan_sdk.sh --release <VERSION> <component,component,...> [options]"
+  echo "usage for SDK version:"
+  echo "   ./install_vulkan_sdk.sh --config <sdk-specs.json> <component,component,...> [options]"
+  echo ""
+  echo "  options:"
+  echo "    --help                this help text"
+  echo "    --query-versions [os] check web services API for available <os>-specific versions (windows|macos|linux; defaults to $os)"
+  echo "    --query-config        check web services API to find corresponding branches/repos for the specified version "
+  echo "    --dry-run             resolve parameters using webservice and display what repos/branches would be installed"
+  echo "    --config <file.json>  use local SDK specs file (instead of querying webservice API)"
+  echo "    --release <VERSION>   Vulkan SDK release number (eg: 1.2.162.1); default: latest"
+  echo ""
+  echo "    available components (default: headers loader):"
+  echo ""
+  for x in $(grep "if\ has_component" $0 | awk '{ print $3 }') ; do
+    echo "      $x"
+  done
+  echo ""
+  echo "for more information please see: https://github.com/humbletim/setup-vulkan-sdk"
+  exit 0
 fi
 
-if [[ $VK_VERSION == sdk-*.*.* ]] ; then
-  echo "using specified branch/tag name as-is: $VK_VERSION" >&2
-  BRANCH=$VK_VERSION
-else
-  # convert an official SDK Release Number into an actual git commit tag (eg: 1.2.162.1 => sdk-1.2.162)
-  BRANCH=$(curl -s https://vulkan.lunarg.com/sdk/config/$VK_VERSION/$os/config.json | jq '.repos["Vulkan-Headers"].branch' --raw-output)
-fi
+function log() { echo $* >&2 ; }
 
-echo BRANCH=$BRANCH >&2
+COMPONENT_MAPING=$(cat << EOF
+{
+  "Vulkan-Headers": "headers",
+  "Vulkan-Loader": "loader",
+  "Vulkan-ValidationLayers": "validation",
+  "Glslang": "glslang",
+  "SPIRV-Cross": "spirv-cross",
+  "SPIRV-Tools": "spirv-tools",
+  "SPIRV-Headers": "spirv-headers",
+  "SPIRV-Reflect": "spirv-reflect",
 
-if [[ $BRANCH == null ]] ; then
-  echo "error: could not resolve $VK_VERSION ($REAL_VK_VERSION) into a git branch via Vulkan SDK service" >&2
-  echo "raw CURL output (https://vulkan.lunarg.com/sdk/config/$VK_VERSION/$os/config.json)" >&2
-  echo "-------------------------------------------------------" >&2
-  curl -i https://vulkan.lunarg.com/sdk/config/$VK_VERSION/$os/config.json >&2
-  echo -e "\n-------------------------------------------------------" >&2
-  echo "NOTE -- according to the web service, these versions are available for os=$os:" >&2
+  "TODO": {
+    "Khronos-Tools": "Khronos-Tools",
+    "LunarG-Tools": "LunarG-Tools",
+    "VulkanSamples": "VulkanSamples",
+    "shaderc": "shaderc",
+    "DXC": "DXC",
+    "gfxreconstruct": "gfxreconstruct",
+    "Vulkan-Docs": "Vulkan-Docs"
+  }
+}
+EOF
+)
+log os=$os
+log build_dir=$build_dir
+
+ARGS=()
+VK_VERSION=
+VK_CONFIG_FILE=
+DRY_RUN=
+QUERY_VERSIONS=
+QUERY_CONFIG=
+VULKAN_COMPONENTS=( "headers loader" )
+
+# process command-line arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --dry-run) DRY_RUN="(dry run)" && shift ;;
+    --config) VK_CONFIG_FILE=$2 && shift 2 ;;
+    --release) VK_VERSION=$2 && shift 2 ;;
+    --query-versions)
+      QUERY_VERSIONS=$os && shift
+      case $1 in windows|mac|linux) QUERY_VERSIONS=$1 ; shift ;; esac
+      ;;
+    --query-config) QUERY_CONFIG=1 && shift 1 ;;
+    -*|--*) echo "Unknown option $1" ;  exit 1 ;;
+    *) ARGS+=("$1") && shift ;;
+  esac
+done
+set -- "${ARGS[@]}" # restore positional parameters
+
+[[ -n $DRY_RUN ]] && log "DRY RUN MODE"
+# [[ -n "$1" ]] && { VK_VERSION=$1 ; shift ; }
+[[ -n "$1" ]] && VULKAN_COMPONENTS=( " ${*//,/ } " )
+
+export MAKEFLAGS=-j2
+
+# resolve logical version into SDK release number
+function resolve_sdk_version() {
+  if [[ $1 == latest ]] ; then
+    log -e "resolving '$1' via Vulkan web services API\n  https://vulkan.lunarg.com/sdk/latest.json"
+    curl -s https://vulkan.lunarg.com/sdk/latest.json | jq .$os --raw-output
+  else
+    echo $1
+  fi
+}
+
+function init() {
+  if [[ -d $GITHUB_ACTION_PATH ]] ; then
+    # set up os-specific prerequisites
+    case $os in
+      mac) ;;
+      linux) . $GITHUB_ACTION_PATH/prereqs/linux.sh ;;
+      windows) . $GITHUB_ACTION_PATH/prereqs/windows-2021.sh ;;
+      *)
+        fail_if_action "unrecognized os: $os uname=='`uname -s`'"
+        exit 10
+        ;;
+    esac
+  fi
+
+  if [[ -n $QUERY_VERSIONS ]] ; then
+    QUERY_URL=https://vulkan.lunarg.com/sdk/versions/$QUERY_VERSIONS.json
+    log "fetching $QUERY_URL"
+    curl -s $QUERY_URL | jq --raw-output '.[]' \
+      || fail_if_action "could not fetch $QUERY_URL"
+    exit 0
+  fi
+
+  [[ -d VULKAN_SDK ]] || mkdir VULKAN_SDK || fail_if_action
+  export VULKAN_SDK=$build_dir/VULKAN_SDK
+
+  if [[ -n "$VK_CONFIG_FILE" ]] ; then
+    [[ -f "$VK_CONFIG_FILE" ]] || fail_if_action "config file not found: '$VK_CONFIG_FILE'"
+      # version is an existing config.json file -- use directly
+    config_json=$(realpath "$VK_CONFIG_FILE")
+    log -e "using specified config.json:\n  $config_json"
+    TARGET_VK_VERSION=$(jq '.version' --raw-output $config_json)
+  else
+    # version assumed to be SDK release number or 'latest' -- resolve using webservice
+    TARGET_VK_VERSION=$(resolve_sdk_version $VK_VERSION)
+    # fetch config.json
+    config_json=$VULKAN_SDK/config.json
+    config_url=https://vulkan.lunarg.com/sdk/config/${TARGET_VK_VERSION}/${os}/config.json
+    log -e "asking Vulkan API webservice for ${TARGET_VK_VERSION} config => $config_json:\n  $config_url"
+    curl -s -o $config_json $config_url || fail_if_action "could not fetch $config_url"
+    if [[ ! -f $config_json || $(jq '.ok' --raw-output $config_json) == 'false' ]] ; then
+      display_vulkan_services_error
+      exit 5
+    fi
+  fi
+
+  if [[ -n $QUERY_CONFIG ]] ; then
+    echo -e "id\tref\trepo\tname"
+    jq --argjson remap "$COMPONENT_MAPING" -r '.repos | to_entries[] | select($remap[.key]) | ($remap[.key] // .key) +"\t" +"\t" + (.value.commit // .value.branch // .value.tag) + "\t" + .value.url +"\t"+.key ' $config_json 
+    exit 0
+  fi
+
+  export VULKAN_SDK_VERSION=$TARGET_VK_VERSION
+  components=( " ${VULKAN_COMPONENTS} " )
+  [[ -d VULKAN_SDK/_build ]] || mkdir VULKAN_SDK/_build
+
+  # prebuilt vulkan SDKs do not include a vulkan loader (eg: vulkan-1.dll on windows)
+  # if requested still build that part manually even when using a prebuilt SDK
+  if true && has_component prebuilt && has_component loader ; then
+    log "NOTE: manually building a compatible vulkan-1.dll to use with prebuilt SDK"
+    components=${components/ loader / prebuilt+loader }
+  fi
+  
+  log "=============================================================="
+  log VK_VERSION=$VK_VERSION
+  log VK_CONFIG_FILE=$VK_CONFIG_FILE
+  log CONFIG_VK_VERSION=$(jq '.version' --raw-output $config_json)
+  log TARGET_VK_VERSION=$TARGET_VK_VERSION
+  log VULKAN_SDK=$VULKAN_SDK
+  log VULKAN_COMPONENTS=$VULKAN_COMPONENTS
+  log components=${components}
+  log config_json=$config_json
+  log "=============================================================="
+  log ""
+
+}
+
+function has_component() {
+  for c in $* ; do
+    [[ " ${components} " =~ " $c " ]] && return 0
+  done
+  return 1
+}
+
+########################################################
+function install_components() {
+
+  # Vulkan-Headers
+  if has_component headers || has_component validation loader ; then
+    push_vulkan_repo_branch Vulkan-Headers
+      run_cmake
+    pop_vulkan_repo_branch headers
+    export VULKAN_HEADERS_INSTALL_DIR=$VULKAN_SDK
+  fi
+
+  # Vulkan-Loader
+  if has_component loader ; then
+    push_vulkan_repo_branch Vulkan-Loader
+      run_cmake
+    pop_vulkan_repo_branch loader
+    export VULKAN_LOADER_INSTALL_DIR=$VULKAN_SDK
+  fi
+
+  # Glslang
+  if has_component glslang || has_component validation; then
+    python3 -V || fail_if_action "building glslangValidator from source requires Python3"
+    push_vulkan_repo_branch Glslang
+      run_cmake
+    pop_vulkan_repo_branch glslang
+    export GLSLANG_INSTALL_DIR=$VULKAN_SDK
+    $VULKAN_SDK/bin/glslangValidator --version    
+  fi
+
+  # SPIRV-Cross
+  if has_component spirv-cross ; then
+    push_vulkan_repo_branch SPIRV-Cross
+      run_cmake
+    pop_vulkan_repo_branch spirv-cross
+  fi
+
+  # SPIRV-Headers
+  if has_component spirv-headers || has_component validation spirv-tools ; then
+    push_vulkan_repo_branch SPIRV-Headers
+      run_cmake
+    pop_vulkan_repo_branch spirv-headers
+    export SPIRV_HEADERS_INSTALL_DIR=$VULKAN_SDK
+  fi
+
+  # SPIRV-Tools
+  if has_component spirv-tools ; then
+    push_vulkan_repo_branch SPIRV-Tools
+      run_cmake -DSPIRV-Headers_SOURCE_DIR=$VULKAN_SDK
+    pop_vulkan_repo_branch spirv-tools
+  fi
+
+  # Vulkan-ValidationLayers
+  if has_component validation ; then
+    push_vulkan_repo_branch Vulkan-ValidationLayers
+      run_cmake
+    pop_vulkan_repo_branch validation
+    export VULKAN_VALIDATIONLAYERS_INSTALL_DIR=$VULKAN_SDK
+  fi
+
+  # SPIRV-Reflect
+  if has_component spirv-reflect ; then
+    push_vulkan_repo_branch SPIRV-Reflect
+      run_cmake
+    pop_vulkan_repo_branch spirv-reflect
+  fi
+
+  # LunarG Prebuilt
+  if has_component prebuilt || has_component prebuilt+loader ; then
+      components=${components/ prebuilt / }
+      vulkan_ext=vulkan_sdk.${ext}
+      log $DRY_RUN download: https://sdk.lunarg.com/sdk/download/${TARGET_VK_VERSION}/${os}/$vulkan_ext?Human=true
+      if [[ ! -n $DRY_RUN ]] ; then
+        [[ -f $vulkan_ext ]] || curl -o $vulkan_ext https://sdk.lunarg.com/sdk/download/${TARGET_VK_VERSION}/${os}/$vulkan_ext?Human=true
+        case $os in
+          linux)
+            # extract just the SDK's prebuilt binaries directly into VULKAN_SDK
+            tar -C $VULKAN_SDK --strip-components 2 -xf $vulkan_ext ${TARGET_VK_VERSION}/x86_64
+            ;;
+          mac)
+            MOUNT=$(hdiutil attach $vulkan_ext | grep vulkansdk | awk 'END {print $NF}')
+            [[ -d $MOUNT ]] || fail_if_action "could not mount dmg image: '$vulkan_ext' (MOUNT=${MOUNT})"
+            [[ -d $MOUNT ]] && log "mounted dmg image: '$vulkan_ext' (MOUNT=${MOUNT})"
+            ls -lR $MOUNT/
+            sudo $MOUNT/InstallVulkan.app/Contents/MacOS/InstallVulkan --root "${VULKAN_SDK}_" --accept-licenses --default-answer --confirm-command install
+            cp -r ${VULKAN_SDK}_/macOS/* ${VULKAN_SDK}/
+            hdiutil detach $MOUNT
+            sudo rm -rf ${VULKAN_SDK}_
+            rm -v $vulkan_ext
+            ;;
+          windows)
+            7z x $vulkan_ext -aoa -o$VULKAN_SDK
+            ls -lR $VULKAN_SDK
+            rm -v $vulkan_ext
+          ;;
+        esac
+      fi
+
+      if has_component prebuilt+loader ; then
+        export VULKAN_HEADERS_INSTALL_DIR=$VULKAN_SDK
+        push_vulkan_repo_branch Vulkan-Loader
+          run_cmake
+        pop_vulkan_repo_branch prebuilt+loader
+        export VULKAN_LOADER_INSTALL_DIR=$VULKAN_SDK
+      fi
+
+  fi
+}
+
+function fail_if_action() {
+  log "==== fail_if_action $*"
+  [[ -d $GITHUB_ACTION_PATH ]] && exit 1
+  return 0
+}
+
+############################################ 
+
+function run_cmake() {
+  [[ -n $DRY_RUN ]] && return 0
+  pushd _build
+    cmake -DCMAKE_INSTALL_PREFIX=$VULKAN_SDK -DCMAKE_BUILD_TYPE=Release .. $*
+    cmake --build . --config Release
+    cmake --install .
+  popd
+}
+
+function push_vulkan_repo_branch() {
+  # log "components='${components}'"
+  # determine corresponding repo and branch/commit point
+  local BRANCH=$(jq '.repos[$component].branch' --arg component "$1" --raw-output $config_json)
+  local COMMIT=$(jq '.repos[$component].commit' --arg component "$1" --raw-output $config_json)
+  local URL=$(jq '.repos[$component].url' --arg component "$1" --raw-output $config_json)
+  local SUBDIR=$(basename -s .git "$URL")
+
+  log "$DRY_RUN cloning $URL @ branch=$BRANCH commit=$COMMIT subdir=$SUBDIR"
+  [[ -n $DRY_RUN ]] && return 0
+  if [[ -n $BRANCH && $BRANCH != null ]] ; then
+    git clone --single-branch "$URL" --branch $BRANCH || fail_if_action
+  elif [[ -n $COMMIT && $COMMIT != null ]] ; then
+    git clone "$URL" >&2 || fail_if_action
+    git -C $SUBDIR checkout $COMMIT
+  else
+    display_vulkan_services_error
+    log -e "\n... aborting"
+    exit 1
+  fi
+  [[ -d $SUBDIR/_build ]] || mkdir $SUBDIR/_build >&2
+  pushd $SUBDIR >&2
+}
+
+function pop_vulkan_repo_branch() {
+  components=${components/ $1 / }
+  # log "//components='${components}'"
+  [[ -n $DRY_RUN ]] && return 0
+  popd >&2
+}
+############################################ 
+
+function display_vulkan_services_error() {
+  log "error: could not resolve $VK_VERSION ($TARGET_VK_VERSION) into a git branch via Vulkan SDK service"
+  log "raw CURL output:"
+  log "-------------------------------------------------------"
+  cat $config_json >&2
+  log -e "\n-------------------------------------------------------"
+  log "NOTE -- according to the web service, these versions are available for os=$os:"
   curl -s https://vulkan.lunarg.com/sdk/versions/$os.json | jq --raw-output '.[]' >&2
-  echo -e "\n... aborting" >&2
-  exit 1
+}
+
+############################################ 
+
+init
+pushd VULKAN_SDK/_build > /dev/null
+  install_components
+popd > /dev/null
+
+[[ $components =~ [[:alpha:]] ]] && fail_if_action "unknown component(s): '$components'"
+
+[[ -n $DRY_RUN ]] && exit 0
+
+# export environment variables for github actions
+if [[ -n "$GITHUB_ENV" ]] ; then
+  echo VULKAN_SDK_VERSION=$VULKAN_SDK_VERSION | tee -a $GITHUB_ENV
+  echo VULKAN_SDK=$VULKAN_SDK | tee -a $GITHUB_ENV
 fi
 
-MAKEFLAGS=-j2
+if [[ -d "$GITHUB_ACTION_PATH" ]] ; then
+  # cleanup _build artifacts
+  [[ -d VULKAN_SDK/_build ]] && rm -rf VULKAN_SDK/_build
+  [[ -d VULKAN_SDK/_vulkan_tmp ]] && rm -rf VULKAN_SDK/_vulkan_tmp
+fi
 
-test -d VULKAN_SDK/_build || mkdir VULKAN_SDK/_build
-pushd VULKAN_SDK/_build >&2
-
-  git clone https://github.com/KhronosGroup/Vulkan-Headers.git --branch $BRANCH >&2
-  pushd Vulkan-Headers >&2
-    cmake -DCMAKE_INSTALL_PREFIX=../.. -DCMAKE_BUILD_TYPE=Release . >&2
-    cmake --build . --config Release >&2
-    cmake --install . >&2
-  popd >&2
-
-  git clone https://github.com/KhronosGroup/Vulkan-Loader.git --branch $BRANCH >&2
-  pushd Vulkan-Loader >&2
-    cmake -DVULKAN_HEADERS_INSTALL_DIR=../.. -DCMAKE_INSTALL_PREFIX=../.. -DCMAKE_BUILD_TYPE=Release . >&2
-    cmake --build . --config Release >&2
-    cmake --install . >&2
-  popd >&2
-popd >&2
-
-echo "" >&2
-
-# export these so that "sourcing" this file directly also works
-export VULKAN_SDK_VERSION=$BRANCH
-
-export VULKAN_SDK=$build_dir/VULKAN_SDK
-
-# also print to STDOUT for eval'ing or appending to $GITHUB_ENV:
-echo VULKAN_SDK_VERSION=$VULKAN_SDK_VERSION
-echo VULKAN_SDK=$VULKAN_SDK
-
-# cleanup _build artifacts which are no longer needed after cmake --installs above
-rm -rf VULKAN_SDK/_build >&2
-
-# echo "VULKAN_SDK/" >&2
-# ls VULKAN_SDK >&2
 du -hs VULKAN_SDK >&2
