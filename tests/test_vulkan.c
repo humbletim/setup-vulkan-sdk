c25f41106918cde0bf347e6f201277392b3a9e9d
humbletim <humbletim@gmail.com>
2025-03-14 15:33:02 -0400

    update test; update to gha-setup-vsdevenv@v5

--- tests/test_vulkan.c
+++ tests/test_vulkan.c
@@ -3,17 +3,17 @@
 #include <stdint.h>
 
 int main(int argc, char** argv) {
-  fprintf(stderr, "main....\n");fflush(stderr);
+  fprintf(stderr, "[test_vulkan.c] main....\n");fflush(stderr);
   uint32_t version = 0;
   PFN_vkEnumerateInstanceVersion vkEnumerateInstanceVersion = (PFN_vkEnumerateInstanceVersion)vkGetInstanceProcAddr(NULL, "vkEnumerateInstanceVersion");
   if(vkEnumerateInstanceVersion) {
       vkEnumerateInstanceVersion(&version);
   } else {
-      fprintf(stderr, "vkGetInstanceProcAddr (%p) lookup of vkEnumerateInstanceVersion failed...\n", vkEnumerateInstanceVersion);
+      fprintf(stderr, "[test_vulkan.c] vkGetInstanceProcAddr (%p) lookup of vkEnumerateInstanceVersion failed...\n", vkEnumerateInstanceVersion);
       return 1;
   }
 
-  printf("Vulkan Version: (%u) %d.%d.%d\n", version,
+  printf("[test_vulkan.c] Vulkan Version: (%u) %d.%d.%d\n", version,
     VK_VERSION_MAJOR(version),
     VK_VERSION_MINOR(version),
     VK_VERSION_PATCH(version)
